02cd6a9f424f8b7503c2098a4886156f0b5b91b5
jathurT <ktmjathur2001@gmail.com>
2025-05-25 09:44:58 +0530

    types of the onjects have been created

---
+++ src/types/course.ts
@@ -0,0 +1,7 @@
+export interface Course {
+    courseName: string;
+    courseCode: string;
+    credits: number;
+    semester: string;
+    departmentName: string;
+}
---
+++ src/types/student.ts
@@ -0,0 +1,8 @@
+export interface CreateStudent {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    dateOfBirth: Date;
+}
