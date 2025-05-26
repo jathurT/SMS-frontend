0b520a79e9b19388eb19773d759cf4cb41e95ad5
Jathurshan Thadchanamoorthy <jathurshant@boardpac.local>
2025-05-26 16:59:53 +0530

    Analytic api has been added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { DepartmentProvider } from '@/contexts/departmentContext'
+import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DepartmentProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>      
     </DepartmentProvider>
   </StrictMode>,
 )
--- src/types/lecturer.ts
+++ src/types/lecturer.ts
@@ -1,3 +1,13 @@
+export interface Lecturer {
+    lecturerId: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber?: string;
+    departmentId: number;
+    address?: string;
+    dateOfBirth: Date;
+}
 export interface CreateLecturer {
     firstName: string;
     lastName: string;
